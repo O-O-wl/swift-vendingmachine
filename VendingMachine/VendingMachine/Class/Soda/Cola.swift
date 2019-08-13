322b945850c1cef1e76c93e6c07dac0389e16932
부엉이 <o.o.wl.soft@gmail.com>
2019-08-14 03:50:36 +0900

    feat: Define Cola

--- VendingMachine/VendingMachine/Class/Soda/Cola.swift
+++ VendingMachine/VendingMachine/Class/Soda/Cola.swift
@@ -8,6 +8,30 @@
 
 import Foundation
 
-class Coke: Soda {
+class Cola: Soda {
+    // MARK: - Properties
+    private let cocaberryContent: Int
     
+    var isAddictive: Bool {
+        return cocaberryContent > 10
+    }
+    
+    // MARK: - Methods
+    init(brand: String = "제조사",
+         capacity: Int = 0,
+         price: Int = 0,
+         name: String = "콜라",
+         productDate: Date = Date(),
+         isSugerFree: Bool = false,
+         cocaberryContent: Int = 20) {
+        self.cocaberryContent = cocaberryContent
+        
+        super.init(
+            brand: brand,
+            capacity: capacity,
+            price: price,
+            name: name,
+            productDate: productDate,
+            isSugerFree: isSugerFree)
+    }
 }
