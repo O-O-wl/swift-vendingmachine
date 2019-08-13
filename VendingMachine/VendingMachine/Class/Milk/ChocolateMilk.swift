bd32a12e640aa22c931701445a61295e4217be41
부엉이 <o.o.wl.soft@gmail.com>
2019-08-14 03:51:31 +0900

    feat: Define ChocolateMilk

--- VendingMachine/VendingMachine/Class/Milk/ChocolateMilk.swift
+++ VendingMachine/VendingMachine/Class/Milk/ChocolateMilk.swift
@@ -1,5 +1,5 @@
 //
-//  ChocolateMilk.swift
+//  SweetMilk.swift
 //  VendingMachine
 //
 //  Created by 이동영 on 14/08/2019.
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class ChocolateMilk: Milk {
+    // MARK: - Properties
+    private let chocolateContent: Int
+    
+    // MARK: - Methods
+    init(
+        brand: String = "제조사",
+        capacity: Int = 0,
+        price: Int = 0,
+        name: String = "초코 우유",
+        productDate: Date = Date(),
+        hasLowFat: Bool = false,
+        hasLactase: Bool = false,
+        chocolateContent: Int = 30) {
+        self.chocolateContent = chocolateContent
+        
+        super.init(
+            brand: brand,
+            capacity: capacity,
+            price: price,
+            name: name,
+            productDate: productDate,
+            hasLowFat: hasLowFat,
+            hasLactase: hasLactase)
+    }
+    
+}
+
