8aab5844036cafb53ffff10f916b005741afc822
부엉이 <o.o.wl.soft@gmail.com>
2019-08-14 03:16:35 +0900

    feat: Define EnergyDrink

---
+++ VendingMachine/VendingMachine/Class/Soda/EnergyDrink.swift
@@ -0,0 +1,37 @@
+//
+//  RedBull.swift
+//  VendingMachine
+//
+//  Created by 이동영 on 14/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class EnergyDrink: Soda {
+    private let caffeineContent: Int
+
+    var isHighCaffeine: Bool {
+        return 100 < self.caffeineContent
+    }
+    
+    // MARK: - Methods
+    init(brand: String = "제조사",
+         capacity: Int = 0,
+         price: Int = 0,
+         name: String = "에너지 드링크",
+         productDate: Date = Date(),
+         isSugerFree: Bool = false,
+         caffeineContent: Int = 50) {
+        self.caffeineContent = caffeineContent
+        
+        super.init(
+            brand: brand,
+            capacity: capacity,
+            price: price,
+            name: name,
+            productDate: productDate,
+            isSugerFree: isSugerFree)
+    }
+    
+}
