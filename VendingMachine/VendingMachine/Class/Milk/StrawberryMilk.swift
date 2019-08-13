b23145c887fe2191beb61637983ef02900fe92c9
부엉이 <o.o.wl.soft@gmail.com>
2019-08-13 16:53:59 +0900

    feat: Define CafeLatte

---
+++ VendingMachine/VendingMachine/Class/Milk/StrawberryMilk.swift
@@ -0,0 +1,13 @@
+//
+//  StrawberryMilk.swift
+//  VendingMachine
+//
+//  Created by 이동영 on 13/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class StrawberryMilk: Milk {
+    
+}
