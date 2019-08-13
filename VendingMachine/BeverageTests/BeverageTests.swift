607e8f3376c41732a511229c74953e3bacb4d4e7
부엉이 <o.o.wl.soft@gmail.com>
2019-08-13 15:39:19 +0900

    feat: Define Coffee

--- VendingMachine/BeverageTests/BeverageTests.swift
+++ VendingMachine/BeverageTests/BeverageTests.swift
@@ -83,5 +83,29 @@ class BeverageTests: XCTestCase {
         //Then
         XCTAssertEqual(result, expected)
     }
+    
+    func testCoffeeDescription() {
+        
+        //Given
+        let brand = "코드스쿼드"
+        let capacity = 200
+        let price = 500
+        let name = "커피"
+        let current =  Date()
+        
+        let beverage = Coffee(
+            brand: brand,
+            capacity: capacity,
+            price: price,
+            name: name,
+            productDate: current)
+        let expected = "\(brand), \(capacity)ml, \(price)원, \(name), \(current.text)"
+        
+        //When
+        let result = beverage.description
+        
+        //Then
+        XCTAssertEqual(result, expected)
+    }
 
 }
