7ffd47f410c90c2aeba91f9fff02fb67c01517a7
부엉이 <o.o.wl.soft@gmail.com>
2019-08-13 15:39:19 +0900

    feat: Define Coffee

--- VendingMachine/VendingMachine/Class/Coffee/Coffee.swift
+++ VendingMachine/VendingMachine/Class/Coffee/Coffee.swift
@@ -1,5 +1,5 @@
 //
-//  Soda.swift
+//  Coffee.swift
 //  VendingMachine
 //
 //  Created by 이동영 on 13/08/2019.
@@ -8,16 +8,19 @@
 
 import Foundation
 
-class Soda: Beverage {
+class Coffee: Beverage {
     private let isSugerFree: Bool
+    private let coffeeBean: CoffeeBean
     
     init(brand: String = "제조사",
          capacity: Int = 0,
          price: Int = 0,
          name: String = "음료",
          productDate: Date = Date(),
-         isSugerFree: Bool = false) {
+         isSugerFree: Bool = false,
+         coffeeBean: CoffeeBean = .arabica) {
         self.isSugerFree = isSugerFree
+        self.coffeeBean = coffeeBean
         
         super.init(
             brand: brand,
@@ -25,5 +28,13 @@ class Soda: Beverage {
             price: price,
             name: name,
             productDate: productDate)
+    }
+}
+// MARK: - + Nested Type CoffeeBean
+extension Coffee {
+    enum CoffeeBean {
+        case arabica
+        case robusta
+        case luwak
     }
 }
