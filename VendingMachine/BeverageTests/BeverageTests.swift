6a26195b48e9ecc426d6a237a4ba71020be94034
부엉이 <o.o.wl.soft@gmail.com>
2019-08-13 15:29:26 +0900

    feat: Define Soda

--- VendingMachine/BeverageTests/BeverageTests.swift
+++ VendingMachine/BeverageTests/BeverageTests.swift
@@ -39,13 +39,37 @@ class BeverageTests: XCTestCase {
     func testMilkDescription() {
         
         //Given
-        let brand = "서울우유"
+        let brand = "코드스쿼드"
         let capacity = 200
         let price = 500
-        let name = "딸기우유"
+        let name = "우유"
         let current =  Date()
         
         let beverage = Milk(
+            brand: brand,
+            capacity: capacity,
+            price: price,
+            name: name,
+            productDate: current)
+        let expected = "\(brand), \(capacity)ml, \(price)원, \(name), \(current.text)"
+        
+        //When
+        let result = beverage.description
+        
+        //Then
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testSodaDescription() {
+        
+        //Given
+        let brand = "코드스쿼드"
+        let capacity = 200
+        let price = 500
+        let name = "탄산음료"
+        let current =  Date()
+        
+        let beverage = Soda(
             brand: brand,
             capacity: capacity,
             price: price,
