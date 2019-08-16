f333d4474cee31d9dcd6c4f1c56086af693ab90a
부엉이 <o.o.wl.soft@gmail.com>
2019-08-17 00:13:17 +0900

    feat: Add Operation overloading

--- VendingMachine/VendingMachine/Structure/Money.swift
+++ VendingMachine/VendingMachine/Structure/Money.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+struct Money {
+    enum Unit: CustomStringConvertible {
+        var description: String {
+            switch self {
+            case .won:
+                return "원"
+            case .dollar:
+                return "$"
+            case .euro:
+                return "€"
+            }
+        }
+        
+        case won
+        case dollar
+        case euro
+    }
+    let unit: Unit = .won
+    var value: Int
+    
+}
+// MARK: - + CustomStringConvertible
+extension Money: CustomStringConvertible {
+    var description: String {
+        let numberFormmater = NumberFormatter()
+        numberFormmater.numberStyle = .decimal
+        let price = numberFormmater.string(from: NSNumber(value: value)) ?? "\(value)"
+        return price + unit.description
+    }
+    
+}
+// MARK: - + Operation overloading
+extension Money: Equatable {
+
+    static func + ( lhs: Money, rhs: Money) -> Money {
+        return Money(value: lhs.value + rhs.value)
+    }
+    
+    static func - ( lhs: Money, rhs: Money) -> Money {
+        return Money(value: lhs.value - rhs.value)
+    }a
+}
