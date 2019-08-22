b15472f50e2badd389797f9394fb596b693d2d9b
부엉이 <o.o.wl.soft@gmail.com>
2019-08-22 23:59:56 +0900

    feat: Add search method

---
+++ VendingMachine/VendingMachine/Structure/Strategy/StrategyFactory.swift
@@ -0,0 +1,13 @@
+//
+//  StrategyGenerator.swift
+//  VendingMachine
+//
+//  Created by 이동영 on 22/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct StrategyGenerator {
+    func make
+}
