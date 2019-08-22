9bb4316aba78ace5855e8b2fc2162351514dc751
부엉이 <o.o.wl.soft@gmail.com>
2019-08-23 00:00:36 +0900

    feat: Define StrategyFactory

--- VendingMachine/VendingMachine/Structure/Strategy/StrategyFactory.swift
+++ VendingMachine/VendingMachine/Structure/Strategy/StrategyFactory.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
-struct StrategyGenerator {
-    func make
+struct StrategyFactory {
+    static func create(input: Input) -> StateHandleable? {
+        switch input {
+        case ("1", let value):
+            let money = Money(value: Int(value)!)
+            return MoneyInsertStrategy(moneyToAdd: money)
+        case ("2", let value):
+            let index = Int(value)!
+            return PurchaseStrategy(productToPurchaseIndex: index)
+        default:
+            return nil
+        }
+    }
 }
