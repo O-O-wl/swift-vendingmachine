49d193f2fcb0362001ce8dd605b27bf9cb16ffaf
부엉이 <o.o.wl.soft@gmail.com>
2019-08-22 22:39:11 +0900

    refactor: Modify Statistic type [String: Int] -> [(String, Int)]

--- VendingMachine/VendingMachine/Structure/VendingMachine.swift
+++ VendingMachine/VendingMachine/Structure/VendingMachine.swift
@@ -12,36 +12,60 @@ typealias State = (balence: Money, inventory: Inventory)
 
 protocol StatePrintable {
     func handleBalence(_ handler: (Money) -> Void)
-    func handleStatistic(_ handler: ([String: Int]) -> Void)
+    func handleStatistic(_ handler: ([(String, Int)]) -> Void)
 }
 
-struct VendingMachine: StatePrintable {
+struct VendingMachine {
     
-    private var balence: Money
+    private var balance: Money
     private var inventory: Inventory
     private var strategy: StateHandleable?
     
-    init(balence: Money,
+    init(balance: Money,
          inventory: Inventory) {
-        self.balence = balence
+        self.balance = balance
         self.inventory = inventory
     }
     
-    func setStrategy(_ strategy: StateHandleable) {
+    mutating func setStrategy(_ strategy: StateHandleable) {
         self.strategy = strategy
     }
     
-    func handleBalence(_ handler: (Money) -> Void) {
-        handler(balence)
-    }
-    
-    func excute() {
-        let state = (balence:balence, inventory:inventory)
+    mutating func execute() throws { 
+        guard let strategy = strategy else { return }
+        let state = (balence: balance, inventory: inventory)
         let result = strategy.handle(state)
+        try resultHandle(result)
     }
     
-    func handleStatistic(_ handler: ([String: Int]) -> Void) {
-        handler(inventory.statistics())
+    mutating func resultHandle(_ result: Result<State, Error>) throws {
+        switch result {
+        case .success(let newState):
+            self.balance = newState.balence
+            self.inventory = newState.inventory
+        case .failure(let error):
+            throw error
+        }
     }
     
 }
+// MARK: - + StatePrintable
+extension VendingMachine: StatePrintable {
+    
+    func handleBalence(_ handler: (Money) -> Void) {
+        handler(balance)
+    }
+    
+    func handleStatistic(_ handler: ([(String, Int)]) -> Void) {
+        handler(inventory.statistic)
+    }
+    
+    func handleStrategy(_ handler: (StateHandleable) -> Void) {
+        guard
+            let strategy = strategy
+            else { return }
+        handler(strategy)
+    }
+    
+}
+
