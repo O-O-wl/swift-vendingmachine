9b9302e2b95b2c647b0bea77860074748fe1e8fc
부엉이 <o.o.wl.soft@gmail.com>
2019-08-29 22:57:03 +0900

    feat: Implement  StorableFactory

--- VendingMachine/VendingMachine/Structure/Factory/StorableFactory.swift
+++ VendingMachine/VendingMachine/Structure/Factory/StorableFactory.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+struct StorableFactory {
+    
+    static func create(categories: [Category] = Category.allCases, quantity: Int) -> Storable {
+        let beverages = categories
+            .map { [Product](repeating: $0.instance, count: quantity) }
+            .reduce([Product]()) { $0 + $1.map { $0 }}
+        return Inventory(products: beverages)
+    }
+}
+
+enum Category: CaseIterable {
+    case strawberryMilk
+    case chocolateMilk
+    case cola
+    case energyDrink
+    case americano
+    case cafeLatte
+    
+    var instance: Beverage {
+        switch self {
+        case .strawberryMilk:
+            return StrawberryMilk()
+        case .chocolateMilk:
+            return ChocolateMilk()
+        case .cola:
+            return Cola()
+        case .energyDrink:
+            return EnergyDrink()
+        case .americano:
+            return Americano()
+        case .cafeLatte:
+            return CafeLatte()
+        }
+    }
+    
+}
