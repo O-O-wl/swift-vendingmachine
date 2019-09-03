fedd24222a26a1a1bb385eeb93e26864c8f653f8
부엉이 <o.o.wl.soft@gmail.com>
2019-09-03 18:55:37 +0900

    refactor: Separate Request create logic to RequestFactory

--- VendingMachine/VendingMachine/Enum/Request.swift
+++ VendingMachine/VendingMachine/Enum/Request.swift
@@ -11,45 +11,6 @@ import Foundation
 enum Request {
     case insert(amount: Int)
     case purchase(index: Int)
-    case inStock(index: Int)
+    case inStock(product: Product)
     case deStock(index: Int)
-    
-    init(authority: Authority, input: String) throws {
-        let ints = input
-            .split(separator: " ")
-            .map { Int($0) }
-        guard
-            ints.count == 2,
-            let menu = ints[0],
-            let value = ints[1],
-            value > 0
-            else { throw InputError.wrongFormat }
-        switch (authority, menu) {
-        case (.admin, 1):
-            self = .inStock(index: value)
-        case (.admin, 2):
-            self = .deStock(index: value)
-        case (.user, 1):
-            self = .insert(amount: value)
-        case (.user, 2):
-            self = .purchase(index: value)
-        default:
-            throw InputError.invalidMenu
-        }
-    }
-    
-    enum InputError: LocalizedError {
-        case wrongFormat
-        case invalidMenu
-        
-        var errorDescription: String? {
-            switch self {
-            case .wrongFormat:
-                return "올바른 포맷이 아닙니다."
-            case .invalidMenu:
-                return "해당하지 메뉴가 존재하지 않습니다."
-            }
-        }
-    }
-    
 }
