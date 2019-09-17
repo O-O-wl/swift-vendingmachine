8f852a50e73c706bcb74f1b79c8ad13929a6ce4f
부엉이 <o.o.wl.soft@gmail.com>
2019-09-18 00:09:36 +0900

    refactor: Modify main

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,32 +9,29 @@
 import Foundation
 // - MARK: - Set Up
 let products = ProductFactory.createAll(quantity: 3)
-
 let inventory = Inventory(products: products)
-
-var vendingMachine = VendingMachine(
-    balance: Money(value: 0),
-    inventory: inventory,
-    history: History())
-
-var mode: Authority
-var inputView: InputView
-var inputString = ""
-
-inputView = InputViewFactory.create(.none)
-inputString = inputView.fetchInput()
-mode = Authority(input: inputString)
+var vendingMachine = VendingMachine(balance: Money(value: 0),
+                                    inventory: inventory,
+                                    history: History())
 
 while vendingMachine.isOnSale {
-
+    let selectInputView = InputViewFactory.create(.none)
+    let authorityIndex = Int(selectInputView.fetchInput())
+    let authority = Authority(index: authorityIndex)
+    
+    guard
+        authority != .none
+        else { continue }
+    
     vendingMachine.handleProductStatistic(OutputView.showStatistic)
     vendingMachine.handleMoney(OutputView.showBalance)
-
-    inputView = InputViewFactory.create(mode)
-    inputString = inputView.fetchInput()
+    
+    let inputView = InputViewFactory.create(authority)
+    let requestString = inputView.fetchInput()
     
     do {
-        let request = try RequestFactory.create(authority: mode, input: inputString)
+        let request = try RequestFactory.create(authority: authority,
+                                                input: requestString)
         let order = StateHandleableFactory.create(request)
         vendingMachine.setStrategy(order)
         try vendingMachine.execute()
